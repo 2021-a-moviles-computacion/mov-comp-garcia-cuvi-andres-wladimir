be76b0ee1fffcd302abe2383e8782ac1f93b59fb
Andrés García <andresgcuvi7@hotmail.com>
2021-07-21 13:17:16 -0500

    Examen01 - Creación de interfaces

---
+++ Examen01/app/src/main/java/com/example/examen01/MateriasActivity.kt
@@ -0,0 +1,13 @@
+package com.example.examen01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MateriasActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_formulario_materia)
+
+        
+    }
+}
---
+++ Examen01/app/src/main/java/com/example/examen01/adapters/AdapterMaterias.kt
@@ -0,0 +1,4 @@
+package com.example.examen01.adapters
+
+class AdapterMaterias {
+}
