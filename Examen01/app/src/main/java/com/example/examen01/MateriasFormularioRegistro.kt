27351778c19acaa96d82bd5db8b20b69efea5594
Andrés García <andresgcuvi7@hotmail.com>
2021-07-25 16:47:18 -0500

    Examen01 - Hasta botón para crear materias en el formulario

--- Examen01/app/src/main/java/com/example/examen01/MateriasFormularioRegistro.kt
+++ Examen01/app/src/main/java/com/example/examen01/MateriasFormularioRegistro.kt
@@ -2,10 +2,43 @@ package com.example.examen01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import java.io.BufferedReader
 
 class MateriasFormularioRegistro : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_materias_formulario_registro)
+        BaseDeDatos.TablaMateria = SQLiteHelper(this)
+        
+        //Ingreso de datos
+
+        val editTextCodigo = findViewById<EditText>(R.id.edtxtCodigoMateria)
+        val editTextNombre = findViewById<EditText>(R.id.edtxtNombreMateria)
+        val editTextCreditos = findViewById<EditText>(R.id.edtxtCreditosMateria)
+        val editTextAula = findViewById<EditText>(R.id.edtxtAulaMateria)
+        val checkBoxEstado = findViewById<CheckBox>(R.id.checkBoxEstadoMateria)
+
+        //Boton para crear
+        val btnCrearMateria = findViewById<Button>(R.id.btnCrearMateria)
+
+
+        btnCrearMateria.setOnClickListener{
+            if(BaseDeDatos.TablaMateria != null){
+                BaseDeDatos.TablaMateria!!.crearMateriaFormulario(
+                    editTextCodigo.text.toString(),
+                    editTextNombre.text.toString(),
+                    editTextCreditos.text.toString().toInt(),
+                    editTextAula.text.toString(),
+                    checkBoxEstado.isChecked
+                )
+
+                Log.i("bdd", "Materia creada")
+            }
+        }
+
     }
 }
