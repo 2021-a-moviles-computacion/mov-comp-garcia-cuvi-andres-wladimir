6943137217e10d6efc8afa88d30363e87d85c4d7
Andrés García <andresgcuvi7@hotmail.com>
2021-08-17 18:39:05 -0500

    Deber02 - WhatsApp - Hasta crear los 3 fragments

---
+++ Deber02/app/src/main/java/com/example/deber02/ui/main/FragmentEstados.kt
@@ -0,0 +1,21 @@
+package com.example.deber02.ui.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.deber02.R
+
+class FragmentEstados: Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_estados, container, false)
+    }
+
+}
